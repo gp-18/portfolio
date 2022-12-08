469dda70b04c65c4dc10ef2eee74464b0f1ab8fa
gp-18 <parthgupta1803@gmail.com>
2022-12-08 13:16:35 +0530

    the first commit halfway done

---
+++ script.js
@@ -0,0 +1,23 @@
+// sticky navbar while scrolling
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+});
+
+
+// navbar responsive 
+// toggle menu/navbar script
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
+
+
+
+// 
